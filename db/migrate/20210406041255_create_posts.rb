dfea621b589ccaa989229e910104ff7788cf1bea
hashimoto <2525day777@gmail.com>
2021-04-06 17:18:04 +0900

    ２回目のコミット

---
+++ db/migrate/20210406041255_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :nickname
+      t.text :mailaddress
+      t.text :password
+
+      t.timestamps
+    end
+  end
+end
